ab0cedafe8b1d9767f8c9a7b5fb71af56f5b1797
Hendrie Ramdhani <50083363+ramdhanihendrie@users.noreply.github.com>
2024-04-29 17:39:28 +0700

    Remove prefix api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,13 +4,18 @@ use App\Http\Middleware\RoleAuthorization;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        using: function () {
+            Route::middleware('api')
+                ->group(base_path('routes/api.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
